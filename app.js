a6637724378909fc1bfcb9479280094002e29f72
gbudjeakp <gbudjeakp@gmail.com>
2021-05-01 10:33:08 -0500

    fix

--- app.js
+++ app.js
@@ -38,9 +38,12 @@ app.use("/api/checkout", checkoutPayment);
 app.use("/api/itinerary", itineraryRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(__dirname + '../client/build'));
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  app.use(express.static(__dirname + "/client/build"));
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
 }
+
 
 app.use(function (req, res, next) {
   next(createError(404));
